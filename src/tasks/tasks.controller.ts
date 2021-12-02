4fc1a74ac40b7ae5cac1ddfbdf164e40fb11648a
Akshaykumar Panchal <akspanch@WKMZTDE16D0F.global.publicisgroupe.net>
2021-12-02 12:41:15 -0500

    connected to postgres database and refactor the code for compatible with postgres schema

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -8,34 +8,33 @@ import {
   Post,
 } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
-  @Get()
-  getTaskData(): Task[] {
-    return this.taskService.getTaskData();
-  }
+  //   @Get()
+  //   getTaskData(): Task[] {
+  //     return this.taskService.getTaskData();
+  //   }
 
-  @Get('/:id')
-  findTask(@Param('id') id: string): Task {
-    const found = this.taskService.findTask(id);
-    if (!found) {
-      throw new NotFoundException();
-    }
-    return found;
-  }
+  //   @Get('/:id')
+  //   findTask(@Param('id') id: string): Task {
+  //     const found = this.taskService.findTask(id);
+  //     if (!found) {
+  //       throw new NotFoundException();
+  //     }
+  //     return found;
+  //   }
 
-  @Post()
-  createTask(@Body() CreateTaskDto: CreateTaskDto): Task {
-    return this.taskService.createTask(CreateTaskDto);
-  }
+  //   @Post()
+  //   createTask(@Body() CreateTaskDto: CreateTaskDto): Task {
+  //     return this.taskService.createTask(CreateTaskDto);
+  //   }
 
-  @Delete('/:id')
-  deleteTask(@Param('id') id: string): void {
-    return this.taskService.deleteTask(id);
-  }
+  //   @Delete('/:id')
+  //   deleteTask(@Param('id') id: string): void {
+  //     return this.taskService.deleteTask(id);
+  //   }
 }
